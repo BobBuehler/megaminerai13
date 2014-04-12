007973f4dff7a12d46c3e6dc957e0306fe6d77a7
Bob Buehler <bob.buehler@gmail.com>
2014-04-12 13:46:19 -0500

    Point

--- Point.cs
+++ Point.cs
@@ -3,6 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-class Point
+struct Point
 {
+    public int x;
+    public int y;
+
+    public Point()
+    {
+        x = 0;
+        y = 0;
+    }
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public override bool Equals(object obj)
+    {
+        Point o = (Point)obj;
+        return o.x == x && o.y == y;
+    }
+
+    public override int GetHashCode()
+    {
+        return x + y * 40; // Bb.Width;
+    }
+
+    public override string ToString()
+    {
+        return String.Format("({0},{1})", x, y);
+    }
 }
